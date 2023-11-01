7b9f78d6164d3cc2e85fcbce2ffaba1b9baaa27f
e.khaleghian <e.khaleghian@rooberah.co>
2023-11-01 15:17:23 +0330

    Image request title to lowercase

--- pages/imageRequest/index.tsx
+++ pages/imageRequest/index.tsx
@@ -5,7 +5,7 @@ import { Container } from "shared";
 export default function ImageRequest() {
   return (
     <Container sx={{ mt: 50 }}>
-      <Typography variant="h3">Product image Request form</Typography>
+      <Typography variant="h3">Product image request form</Typography>
       <RequestForm />
     </Container>
   );
